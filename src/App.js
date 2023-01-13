64ff1d8ff87c44d940ff433e7acc83cd43520de4
amirsuran <amirhosseinsoran@gmail.com>
2023-01-13 06:29:32 +0330

    23 dey mah commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Fragment } from "react";
+import List from "./components/List";
+import { playlist2 } from "./components/list2";
+import { playlist } from "./components/data";
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
             GayGrizzly
           </a>
         </div>
-        <div className="navbar-text-right">
+        <div className="navbar-text-right font-[Inter] space-x-5 mr-8 ">
           <a
             className="navbar-text-right"
             href="https://www.radiojavan.com/mp3s"
@@ -21,7 +23,7 @@ function App() {
           <a className="navbar-text-right" href="/....">
             Podcasts
           </a>
-          <a className="navbar-text-right" href="">
+          <a className="navbar-text-right" href="/....">
             Playlist
           </a>
           <a className="navbar-text-right" href="/....">
@@ -34,6 +36,7 @@ function App() {
           <img
             src="https://assets.rjassets.com/static/content_items/images/2022/10/27/188892567abb7f6.jpg"
             title="Erfan And Jidal"
+            alt=""
             className="top-img-nav"
           />
           <figcaption className="text-in-img">Erfan & Jidal</figcaption>
@@ -43,10 +46,13 @@ function App() {
             className="top-img-nav"
             src="https://assets.rjassets.com/static/content_items/images/2022/10/28/8e97ad95ceb3e06.jpg"
             title="Maziar Falahi"
+            alt=""
           />
           <figcaption className="text-in-img">Maziar Falahi</figcaption>
         </figure>
       </div>
+      <List data={playlist2} title="this is title" />
+      <List data={playlist} title="awmkd;awmd" />
     </Fragment>
   );
 }
---
+++ src/components/list2.js
@@ -0,0 +1,38 @@
+export const playlist2 = [
+  {
+    id: 0, // Used in JSX as a key
+    name: "Todays top hit",
+    imageId:
+      "https://assets.rjassets.com/static/playlist/5883691/28910480c6580da.jpg",
+  },
+  {
+    id: 1, // Used in JSX as a key
+    name: "Azadi",
+    imageId:
+      "https://assets.rjassets.com/static/playlist/9824311/eb95f1ee8bb1d97.jpg",
+  },
+  {
+    id: 2, // Used in JSX as a key
+    name: "Yalda",
+    imageId:
+      "https://assets.rjassets.com/static/playlist/9712174/52c47ed5b21cd43.jpg",
+  },
+  {
+    id: 3, // Used in JSX as a key
+    name: "2 Nafare",
+    imageId:
+      "https://assets.rjassets.com/static/playlist/1591981/ca6088911484428.jpg",
+  },
+  {
+    id: 4, // Used in JSX as a key
+    name: "Realxing",
+    imageId:
+      "https://assets.rjassets.com/static/playlist/1602549/70a673afbd96feb.jpg",
+  },
+  {
+    id: 5, // Used in JSX as a key
+    name: "Top Songs",
+    imageId:
+      "https://assets.rjassets.com/static/playlist/1612406/912ff779d0cf81f.jpg",
+  },
+];
